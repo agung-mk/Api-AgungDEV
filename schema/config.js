39a6529383bd2714106a76dd32ca1dc85aadbda4
siagungg_ <147239620+agung-mk@users.noreply.github.com>
2024-09-25 16:01:19 +0700

    Update config.js

--- schema/config.js
+++ schema/config.js
@@ -1,8 +1,8 @@
 const options = {
-  name: "HitamAPIs",
-  developer: "@sayuran.vip",
+  name: "AgungDEV APIs",
+  developer: "@siagungg_",
   port: 3000,
-  webName: "HitamAPIs Playground",
+  webName: "AgungDEV APIs Playground",
   description: "Simple Base Rest API",
   favicon:
     "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Black.png/220px-Black.png",
