05e9b8d83d917b688a7aa7525cf02490d9d87ccd
siagungg_ <147239620+agung-mk@users.noreply.github.com>
2024-09-25 14:04:13 +0700

    Update config.js

--- schema/config.js
+++ schema/config.js
@@ -1,16 +1,15 @@
 const options = {
-  name: "HitamAPIs",
-  developer: "@sayuran.vip",
+  name: "AgungDEV APIs",
+  developer: "@siagungg_",
   port: 3000,
-  webName: "HitamAPIs Playground",
+  webName: "AgungDEV APIs Playground",
   description: "Simple Base Rest API",
   favicon:
     "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Black.png/220px-Black.png",
 };
 
 const host = {
-  BASE_URL: "https://api-flax-chi-68.vercel.app", // Ganti dengan URL yang sesuai
-  // Contoh: https://domain.com
+  BASE_URL: "https://api.agungdev.us.kg",
 };
 
 module.exports = {
